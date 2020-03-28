904bb754b958031ff6f3e173f11816205909449e
binio <tbiniecki@gmail.com>
2020-03-28 11:30:49 +0000

    add test templates

---
+++ src/test/java/com/binio/service/OrderServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.binio.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void addOrder() {
+    }
+
+    @Test
+    void getOrderSummary() {
+    }
+}
---
+++ src/test/java/com/binio/service/ProductServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.binio.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductServiceImplTest {
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void getProductBySku() {
+    }
+
+    @Test
+    void getProductBySkuAndProductDeleted() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void createProduct() {
+    }
+}
